9fccb1051488ca6cb00be0a8482530e1884a50e2
zaryab-ahmad-10 <zaryabahmad2005@gmail.com>
2024-07-12 15:56:11 +0500

    Making separate files for each class.

---
+++ Book.py
@@ -0,0 +1,9 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+        author.add_book(self)
+
+    def __str__(self):
+        return f"Book: {self.title}, Author: {self.author.name}"
